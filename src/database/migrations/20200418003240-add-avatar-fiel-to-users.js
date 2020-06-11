10256c80d38fee23e79ce630d0ad847861e91d8e
thiago rossato <thiago.rossato.tr@gmail.com>
2020-06-10 22:50:41 -0300

    corrigindo migrations

--- src/database/migrations/20200418003240-add-avatar-fiel-to-users.js
+++ src/database/migrations/20200418003240-add-avatar-fiel-to-users.js
@@ -10,6 +10,6 @@ module.exports = {
   },
 
   down: (queryInterface) => {
-    return queryInterface.removeColunm('users', 'avatar_id');
+    return queryInterface.removeColumn('users', 'avatar_id');
   },
 };
